73bfc50664c76d27354d10252c4d29c11c374706
Thomas Perez <thomasperez@uagateway.org>
2016-09-26 15:51:14 +0000

    initial commit

---
+++ unit1/tperez_calcCtoF.py
@@ -0,0 +1,3 @@
+celcius = input("Please input the tempature in degrees Celcius:")
+fahrenheit = float(celcius) * 1.8 + 32
+print(str(celcius) + " C is equal to " + str(fahrenheit) + " F")
---
+++ unit1/tperez_calcFtoC.py
@@ -0,0 +1,3 @@
+fahrenheit = input("Please input the tempature in degrees Fahrenheit:")
+celcius = (float(fahrenheit) - 32) / 1.8
+print(str(fahrenheit) + " F is equal to " + str(celcius) + " C")
