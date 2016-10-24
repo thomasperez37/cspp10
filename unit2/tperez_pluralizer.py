79448feed95558b447df4696d68f71c9587a472c
Thomas Perez <thomasperez@uagateway.org>
2016-10-24 01:01:15 +0000

    Made labs 2-1 and 2-2 more readable

--- unit2/tperez_pluralizer.py
+++ unit2/tperez_pluralizer.py
@@ -1,7 +1,10 @@
+#Note: I used two websites that explained plural noun rules very well so I have rules for plural nouns but not every special noun may be included.
 amount = int(input("Input the amount of the noun: "))
 noun = input("Input the singular noun: ")
+#This if statement will determine if the noun is singular
 if amount == 1:
     print("1 {}".format(noun))
+#These longer elif statements are meant to catch a specific group of special words that I couldn't group with a single print statement.
 elif amount != 1 and (noun == "trousers" or noun=="jeans" or noun == "glasses" or noun == "savings" or noun == "thanks" or noun == "steps" or noun == "stairs" or noun == "customs" or noun == "congratulations" or noun == "tropics" or noun == "wages" or noun == "spectacles" or noun == "outskirts" or noun == "goods" or noun == "wits" or noun == "news" or noun == "athletics" or noun == "linguistics" or noun == "billiards"):
     print("{} {}".format(amount,noun))
 elif amount != 1 and (noun == "deer" or noun == "sheep" or noun == "aircraft" or noun == "fish" or noun == "species"):
@@ -10,10 +13,11 @@ elif amount != 1 and (noun == "dwarf" or noun == "roof" or noun == "turf"):
     print("{} {}s".format(amount,noun))
 elif amount != 1 and (noun == "criterion" or noun == "phenomenon"):
     print("{} {}a".format(amount,noun[:-2])) 
+elif amount != 1 and (noun == "man" or noun == "woman"):
+    print("{} {}en".format(amount,noun[:-2]))
+#These short elif statements are for pluralizing some special words that can't be grouped in to many things.
 elif amount != 1 and noun == "child":
     print("{} children".format(amount))
-elif amount != 1 and noun[-2:] == "an":
-    print("{} {}en".format(amount,noun[:-2]))
 elif amount != 1 and noun == "person":
     print("{} people".format(amount))
 elif amount != 1 and noun == "cafe":
@@ -32,6 +36,7 @@ elif amount != 1 and noun == "datum":
     print("{} data".format(amount))
 elif amount != 1 and noun == "bus":
     print("{} buses".format(amount))
+#These set of elif statements group non-specific singular nouns into general rules and pluralizes them.
 elif amount != 1 and noun[-2:] == "is":
     print("{} {}es".format(amount,noun[:-2]))
 elif amount != 1 and noun[-2:] == "us":
--- unit2/tperez_simplecalc.py
+++ unit2/tperez_simplecalc.py
@@ -1,23 +1,21 @@
 equation = input("Input equation with no spaces: ")
-lenfirstterm = int(input("What is the length of the first term? "))
-lensecondterm = int(input("What is the length of the second term? "))
-firstterm = int(equation[:lenfirstterm])
-mathsign = equation[lenfirstterm]
-secondterm = int(equation[-lensecondterm:])
-if mathsign == '+':
-    equalto = firstterm+secondterm
-    print("The result is {}.".format(equalto))
-elif mathsign == '-':
-    equalto = firstterm-secondterm
-    print("The result is {}.".format(equalto))
-elif mathsign == '*':
-    equalto = firstterm*secondterm
-    print("The result is {}.".format(equalto))
-elif mathsign == '/':
-    equalto = firstterm/secondterm
-    print("The result is {}.".format(equalto))
-elif mathsign == '%':
-    equalto = firstterm%secondterm
-    print("The result is {}.".format(equalto))
+#In order to retrieve the terms from this equation, I need the length of them.
+len_first_term = int(input("What is the length of the first term? "))
+len_second_term = int(input("What is the length of the second term? "))
+#Now I can pick out the terms and their math sign.
+first_term = int(equation[:len_first_term])
+math_sign = equation[len_first_term]
+second_term = int(equation[-len_second_term:])
+#These if and elif statements will determine the equation we have. Then, it will print out what it is equal to.
+if math_sign == '+':
+    print("The result is {}.".format(first_term + second_term))
+elif math_sign == '-':
+    print("The result is {}.".format(first_term - second_term))
+elif math_sign == '*':
+    print("The result is {}.".format(first_term * second_term))
+elif math_sign == '/':
+    print("The result is {}.".format(first_term / second_term))
+elif math_sign == '%':
+    print("The result is {}.".format(first_term % second_term))
 else:
     print("Error!")
