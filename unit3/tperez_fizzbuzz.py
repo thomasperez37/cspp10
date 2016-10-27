39710986c7392ecc6938b634915bafb39ea012a1
Thomas Perez <thomasperez@uagateway.org>
2016-10-27 16:16:35 +0000

    Worked even more on lab 3-1

--- unit3/tperez_fizzbuzz.py
+++ unit3/tperez_fizzbuzz.py
@@ -2,11 +2,17 @@ end_point = int(input("Input the end point of the Fizz Buzz sequence:"))
 for num in range(1,end_point+1):
     len_of_num = len(str(num))
     for number in range(len_of_num):
+        num_word = "" + str(num)[number]
         if (str(num)[number] == "3" or num % 3 == 0) and (str(num)[number] == "5" or num % 5 == 0):
             print("FizzBuzz,", end=" ")
+            break
         elif str(num)[number] == "3" or num % 3 == 0:
             print("Fizz,", end=" ")
+            break
         elif str(num)[number] == "5" or num % 5 == 0:
             print("Buzz,", end=" ")
+            break
+        elif num_word != str(num):
+            break
         else:
             print("{},".format(num), end=" ")
