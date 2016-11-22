0eb3aa3d0af9d18676e73c08de6b8976dfde0af5
Thomas Perez <thomasperez@uagateway.org>
2016-11-22 17:17:14 +0000

    I almost finished all of lab 4-1

--- unit4/tperez_rps.py
+++ unit4/tperez_rps.py
@@ -6,6 +6,7 @@ def ask_for_tutorial():
     return tutorial_prompt
         
 def give_tutorial():
+    print("------------------------------")
     print("Wow...")
     print("Uh...well anyway, you and the computer will choose Rock, Paper, or Scissors. Each item is strong against one item and weak against another.")
     print("Rock is strong against Scissors and weak against Paper.")
@@ -38,12 +39,14 @@ def get_comp_move():
 #   purpose: allows the user to choose a number of rounds from 1 to 9.
 #   returns: the user-chosen number of rounds
 def get_rounds():
+    print("------------------------------")
     print("Caution: We need you to input the number of rounds for the game but if you input a letter, an error will occur.")
     num_of_rounds = input("Choose the number of rounds only from [1 to 9]: ")
     while int(num_of_rounds) < 1 or int(num_of_rounds) > 9:
         num_of_rounds = input("Choose the number of rounds ONLY from [1 to 9]: ")
     return int(num_of_rounds)
-        
+   
+
 #function name: get_round_winner
 #   arguments: player move, computer move
 #   purpose: based on the player and computer's move, determine
@@ -91,6 +94,13 @@ def print_score(pscore, cscore, ties):
     print("Computer Score: {}".format(cscore))
     print("Ties: {}".format(ties))
 
+def print_winner(pscore, cscore):
+    if pscore > cscore:
+        print("Based on the results, you won the majority of the rounds! Congratulations!")
+    elif pscore < cscore:
+        print("Based on the results, the computer won the majority of the rounds. Better luck next time.")
+    else:
+        print("Based on the results, no one wins. It's a tie.")
 #function name: rps
 #   arguments: none
 #   purpose: the main game loop.  This should be the longest, using
@@ -98,15 +108,17 @@ def print_score(pscore, cscore, ties):
 #   returns: none
 def rps():
     print("Let's play Rock, Paper, Scissors!")
-    if ask_for_tutorial() == 'y':
+    if ask_for_tutorial() == 'n':
         give_tutorial()
     print("Well then, let\'s begin.")
     p_score = 0
     c_score = 0
-    ties = 0
+    tie_score = 0
     rounds = get_rounds()
     round_number = 1
-    while rounds != 0:
+    score_objective = rounds//2 + 1
+    while p_score != (score_objective-tie_score) and c_score != (score_objective-tie_score) and rounds != 0:
+        print("------------------------------")
         print("Round {}!".format(round_number))
         p_move = get_p1_move()
         c_move = get_comp_move()
@@ -115,14 +127,35 @@ def rps():
         print("Your move: {}".format(full_p_move))
         print("Computer's move: {}".format(full_c_move))
         if get_round_winner(p_move, c_move) == "player":
-            
+            p_score = p_score + 1
+            rounds = rounds - 1
+            print("You won Round {}!".format(round_number))
+            print_score(p_score, c_score, tie_score)
+            round_number = round_number + 1
+        elif get_round_winner(p_move, c_move) == "comp":
+            c_score = c_score + 1
+            rounds = rounds - 1
+            print("The computer won Round {}...".format(round_number))
+            print_score(p_score, c_score, tie_score)
+            round_number = round_number + 1
+        else:
+            tie_score = tie_score + 1
+            rounds = rounds - 1
+            print("You got a tie for Round {}".format(round_number))
+            print_score(p_score, c_score, tie_score)
+            round_number = round_number + 1
+    print("------------------------------")
+    print("Okay! Lets stop there. Here are the final scores.")
+    print_score(p_score, c_score, tie_score)
+    print_winner(p_score, c_score)
+    print("GAME OVER")
 
 #function name: tests
 #   arguments: none
 #   purpose: a place for you to write your tests.  replace 'rps' below
 #               with 'tests' to run this function instead of the game loop
 #   returns: none
-def test():
-    #code here
+
+    
 
 rps()
