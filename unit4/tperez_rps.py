887ab13c41fca715c4e37c846dc1f43983216005
Thomas Perez <thomasperez@uagateway.org>
2016-11-23 17:14:18 +0000

    I finished lab 4-1 and made it PvP

--- unit4/tperez_rps.py
+++ unit4/tperez_rps.py
@@ -1,4 +1,5 @@
 import random
+import getpass
 def ask_for_tutorial():
     tutorial_prompt = input("You do know how to play Rock, Paper, Scissors, right? Input [ y or n ]: ")
     while tutorial_prompt != 'y' and tutorial_prompt != 'n':
@@ -18,21 +19,23 @@ def give_tutorial():
     print("After all the rounds, I'll give you the scoreboard and tell you who won.")
 #function name: get_p1_move
 #   arguments: none
-#   purpose: present player with options, use input() to get player move
+#   purpose: present player with options, use getpass.getpass() to get player move
 #   returns: the player's move as either 'r', 'p', or 's'
 def get_p1_move():
-    your_move = input("Choose your move only from [r, p, s]: ")
-    while your_move != 'r' and your_move != 'p' and your_move != 's':
-        your_move = input("Choose your move ONLY from [r, p, s]: ")
-    return your_move
+    p1_move = getpass.getpass("Player 1, choose your move only from [r, p, s]: ")
+    while p1_move != 'r' and p1_move != 'p' and p1_move != 's':
+        p1_move = getpass.getpass("Choose your move ONLY from [r, p, s]: ")
+    return p1_move
 
-#function name: get_comp_move():
+#function name: get_p2_move():
 #   arguments: none
-#   purpose: randomly generates the computer's move,
-#            either 'r' 'p' or 's'
-#   returns: the computer's randomly generated move
-def get_comp_move():
-    return "rps"[random.randint(0,2)]
+#   purpose: present player with options, use getpass.getpass() to get player move
+#   returns: the player's move as either 'r', 'p', or 's'
+def get_p2_move():
+    p2_move = getpass.getpass("Player 2, choose your move only from [r, p, s]: ")
+    while p2_move != 'r' and p2_move != 'p' and p2_move != 's':
+        p2_move = getpass.getpass("Choose your move ONLY from [r, p, s]: ")
+    return p2_move
 
 #function name: get_rounds
 #   arguments: none
@@ -49,25 +52,25 @@ def get_rounds():
 
 #function name: get_round_winner
 #   arguments: player move, computer move
-#   purpose: based on the player and computer's move, determine
+#   purpose: based on the player 1 and player 2's move, determine
 #            the winner or if it's a tie
 #   returns: returns a string based on the following:
-#               "player" if player won
-#               "comp" if computer won
+#               "player1" if player1 won
+#               "player2" if player2 won
 #               "tie" if it's a tie
-def get_round_winner(p1move, cmove):
-    if p1move == "r" and cmove == "s":
-        return "player"
-    elif p1move == "s" and cmove == "p":
-        return "player"
-    elif p1move == "p" and cmove == "r":
-        return "player"
-    elif p1move == "s" and cmove == "r":
-        return "comp"
-    elif p1move == "p" and cmove == "s":
-        return "comp"
-    elif p1move == "r" and cmove == "p":
-        return "comp"
+def get_round_winner(p1move, p2move):
+    if p1move == "r" and p2move == "s":
+        return "player1"
+    elif p1move == "s" and p2move == "p":
+        return "player1"
+    elif p1move == "p" and p2move == "r":
+        return "player1"
+    elif p1move == "s" and p2move == "r":
+        return "player2"
+    elif p1move == "p" and p2move == "s":
+        return "player2"
+    elif p1move == "r" and p2move == "p":
+        return "player2"
     else:
         return "tie"
 #function name: get_full_move
@@ -86,19 +89,19 @@ def get_full_move(shortmove):
         return 'Paper'
 
 #function name: print_score
-#   arguments: player score, computer score, number of ties
+#   arguments: player 1 score, player 2 score, number of ties
 #   purpose: prints the scoreboard
 #   returns: none
-def print_score(pscore, cscore, ties):
-    print("Player Score: {}".format(pscore))
-    print("Computer Score: {}".format(cscore))
+def print_score(p1score, p2score, ties):
+    print("Player 1 Score: {}".format(p1score))
+    print("Player 2 Score: {}".format(p2score))
     print("Ties: {}".format(ties))
 
-def print_winner(pscore, cscore):
-    if pscore > cscore:
-        print("Based on the results, you won the majority of the rounds! Congratulations!")
-    elif pscore < cscore:
-        print("Based on the results, the computer won the majority of the rounds. Better luck next time.")
+def print_winner(p1score, p2score):
+    if p1score > p2score:
+        print("Based on the results, player 1 won the majority of the rounds! Congratulations Player 1!")
+    elif p1score < p2score:
+        print("Based on the results, player 2 won the majority of the rounds! Congratulations Player 2!")
     else:
         print("Based on the results, no one wins. It's a tie.")
 #function name: rps
@@ -111,43 +114,44 @@ def rps():
     if ask_for_tutorial() == 'n':
         give_tutorial()
     print("Well then, let\'s begin.")
-    p_score = 0
-    c_score = 0
+    p1_score = 0
+    p2_score = 0
     tie_score = 0
     rounds = get_rounds()
     round_number = 1
     score_objective = rounds//2 + 1
-    while p_score != (score_objective-tie_score) and c_score != (score_objective-tie_score) and rounds != 0:
+    while p1_score != score_objective and p2_score != score_objective and rounds != 0:
         print("------------------------------")
         print("Round {}!".format(round_number))
-        p_move = get_p1_move()
-        c_move = get_comp_move()
-        full_p_move = get_full_move(p_move)
-        full_c_move = get_full_move(c_move)
-        print("Your move: {}".format(full_p_move))
-        print("Computer's move: {}".format(full_c_move))
-        if get_round_winner(p_move, c_move) == "player":
-            p_score = p_score + 1
+        p1_move = get_p1_move()
+        p2_move = get_p2_move()
+        full_p1_move = get_full_move(p1_move)
+        full_p2_move = get_full_move(p2_move)
+        print("Player 1's move: {}".format(full_p1_move))
+        print("Player 2's move: {}".format(full_p2_move))
+        if get_round_winner(p1_move, p2_move) == "player1":
+            p1_score = p1_score + 1
             rounds = rounds - 1
-            print("You won Round {}!".format(round_number))
-            print_score(p_score, c_score, tie_score)
+            print("Player 1 won Round {}!".format(round_number))
+            print_score(p1_score, p2_score, tie_score)
             round_number = round_number + 1
-        elif get_round_winner(p_move, c_move) == "comp":
-            c_score = c_score + 1
+        elif get_round_winner(p1_move, p2_move) == "player2":
+            p2_score = p2_score + 1
             rounds = rounds - 1
-            print("The computer won Round {}...".format(round_number))
-            print_score(p_score, c_score, tie_score)
+            print("Player 2 won Round {}!".format(round_number))
+            print_score(p1_score, p2_score, tie_score)
             round_number = round_number + 1
         else:
             tie_score = tie_score + 1
             rounds = rounds - 1
-            print("You got a tie for Round {}".format(round_number))
-            print_score(p_score, c_score, tie_score)
+            print("It was a tie for Round {}.".format(round_number))
+            print_score(p1_score, p2_score, tie_score)
+            score_objective = rounds//2 + 1
             round_number = round_number + 1
     print("------------------------------")
     print("Okay! Lets stop there. Here are the final scores.")
-    print_score(p_score, c_score, tie_score)
-    print_winner(p_score, c_score)
+    print_score(p1_score, p2_score, tie_score)
+    print_winner(p1_score, p2_score)
     print("GAME OVER")
 
 #function name: tests
