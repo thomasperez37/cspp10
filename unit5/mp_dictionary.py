2a62978fa149363b2aa1d08a6bd7dee5c6e1b808
Thomas Perez <thomasperez@uagateway.org>
2017-02-16 17:18:29 +0000

    Finished dictionary assignment

--- unit5/mp_dictionary.py
+++ unit5/mp_dictionary.py
@@ -1,30 +1,48 @@
 from pprint import pprint
 
 def add(dictionary_list):
-    new_key = input()
-    new_value = input()
+    new_key = input("Type in a word for the key of the new key-value pair: ")
+    new_value = input("Type in a word for the value of the new key-value pair: ")
     while new_key in dictionary_list:
-        new_key = input()
-        new_value = input()
+        print("That key already exists!")
+        new_key = input("Type in a different word for the key of the new key-value pair: ")
+        new_value = input("Type in a word for the value of the new key-value pair: ")
     dictionary_list[new_key] = new_value
     
 def remove_key(dictionary_list):
-    selected_key = input()
+    selected_key = input("Type in the word of the key of the key-value pair you want to remove: ")
     if selected_key in dictionary_list:
         del dictionary_list[selected_key]
     
-def remove_value(dictionary_list):
-    selected_value = input()
-    for key in dictionary_list:
-        if dictionary_list[key] == selected_value:
-            del dictionary_list[key]
-    
 def update(dictionary_list):
-    selected_key = input()
+    selected_key = input("Type in the word of the key of the key-value pair you want to change: ")
     if selected_key in dictionary_list:
-        selected_value = input()
-        dictionary_list[selected_key] = selected_value
-        
+        new_value = input("Type in a word for the new value of the key-value pair: ")
+        dictionary_list[selected_key] = new_value
+
 def give_dictionary_options():
     dictionary = {}
-    selected_option = input()
+    print("You have 5 options. Type in \'add\' to be able to add a new key-value pair. However, you won\'t be able to add a new key-value pair that has the same key as another pair.")
+    print("Type in \'remove key\' to be able to remove a key-value pair by typing it\'s corresponding key.")
+    print("Type in \'update\' to be able to change an already existing key-value pair\'s value.")
+    print("Type in \'print\' to print out the dictionary.")
+    print("Type in \'exit\' to end the program.")
+    selected_option = input("Please choose one of the 5 options: ")
+    while selected_option != 'exit':
+        if selected_option == 'add':
+            add(dictionary)
+            selected_option = input("Please choose another one of the 5 options: ")
+        elif selected_option == 'remove key':
+            remove_key(dictionary)
+            selected_option = input("Please choose another one of the 5 options: ")
+        elif selected_option == 'update':
+            update(dictionary)
+            selected_option = input("Please choose another one of the 5 options: ")
+        elif selected_option == 'print':
+            pprint(dictionary)
+            selected_option = input("Please choose another one of the 5 options: ")
+        elif selected_option == 'exit':
+            print("PROGRAM END")
+        else:
+            print("Not a valid option")
+            selected_option = input("Please choose another one of the 5 options: ")
