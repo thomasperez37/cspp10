32197c01ebb3202a521e19dd748db0d1c0565f2b
Thomas Perez <thomasperez@uagateway.org>
2017-02-19 21:22:15 +0000

    I finished the scramble project errors without using shuffle

--- unit5/tperez_scramble.py
+++ unit5/tperez_scramble.py
@@ -1,4 +1,4 @@
-# import random https://docs.google.com/document/d/1O8tJ3CZ7pSJCjCd1kCQ-ekdmxZI-6csQ7jzU1AwIi2A/edit
+# import random 
 # define a function that only scrambles 4 letter long words,
 # turns the strings into lists, does not shuffle the first and last letter,
 # and returns the scrambled word
@@ -9,7 +9,7 @@
 # through string.split()
 # then i'll use a for loop to
 # go through each word and scramble each word using the first function
-# to perform the code, i'll simply call the second function
+# to perform the code, i'll simply print the second function
 # challenge notes: in order to shuffle a word without shuffle()
 # then i need to use random.randint() to pick random indexes according to 
 # the length of the word list
@@ -24,9 +24,15 @@ def scramble_word(word_item):
         all_letters = list(word_item)
         first_letter = all_letters[:1]
         last_letter = all_letters[-1:]
-        remaining_letters = all_letters[1:len(all_letters)-1]#edit this mores
-        remaining_letters_copy1 = ''.join(remaining_letters)#edit this more
+        remaining_letters = all_letters[1:len(all_letters)-1]
+        remaining_letters_copy1 = ''.join(remaining_letters)
         remaining_letters_copy2 = ''.join(remaining_letters)
+        #I just noticed that if all the letters in remaining_letters are
+        #all the same, the middle part of the word could never change
+        #therefore, i will simply return the word unscrambled in
+        #such cases
+        if all(x == remaining_letters[0] for x in remaining_letters):
+            return word_item
         while remaining_letters_copy1 == remaining_letters_copy2:
             for scramble_times in range(5):
                 index1 = random.randint(0,len(remaining_letters)-1)
@@ -36,18 +42,16 @@ def scramble_word(word_item):
                     index2 = random.randint(0,len(remaining_letters)-1)
                 swap(remaining_letters,index1,index2)
             remaining_letters_copy2 = ''.join(remaining_letters)
-        new_letters = remaining_letters
-        new_letters.insert(0,first_letter)
-        new_letters.append(last_letter)
+        new_letters = first_letter + remaining_letters + last_letter
         new_word = ''.join(new_letters)
         return new_word
     return word_item
     
 def scramble_phase(phrase):
     word_list = phrase.split(" ")
-    for word in word_list:
-        word = scramble_word(word)
+    for index in range(len(word_list)):
+        word_list[index] = scramble_word(word_list[index])
     new_phrase = " ".join(word_list)
     return new_phrase
     
-scramble_phase(input("Please enter the phrase you want scrambled: "))
+print(scramble_phase(input("Please enter the phrase you want scrambled: ")))
